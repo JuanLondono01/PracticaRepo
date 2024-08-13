2dd8446a7dfda85be11dc80cf0cc7c30bb3b4357
Andermc10 <andermuri94@gmail.com>
2024-08-13 06:56:49 -0500

    navbar added

--- mensaje.js
+++ mensaje.js
@@ -1,1 +1,10 @@
-alert("Estoe es una alerta")
+const nav = document.querySelectorAll(".nav-item");
+
+nav.forEach(item => {
+    item.addEventListener("click", () => {
+        // Remove 'active' class from all nav items
+        nav.forEach(navItem => navItem.classList.remove("active"));
+        // Add 'active' class to the clicked nav item
+        item.classList.add("active");
+    });
+});
